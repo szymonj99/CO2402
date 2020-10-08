c06ad400821816123d58197890edd30d173af087
szymonj99 <szymon.j@hotmail.com>
2020-10-08 11:15:25 +0100

    Add week 3

---
+++ Week3/pattern-debugging/CPattern.cpp
@@ -0,0 +1,99 @@
+// week 3: debug 
+#include <iostream>
+using namespace std;
+
+const int TOP = 6;
+
+class CPattern
+{
+private:
+	int mSequence[TOP];
+public:
+	void Initialise(int sequence[TOP]);
+	void Display();
+	int FindMax();
+	void DisplayReversed();
+	int FindLocation(int value);
+};
+
+// Initialise the array.
+// Better to do this as a constructor - if you are confident about constructors then
+// convert this method into one
+void CPattern::Initialise(int sequence[TOP])
+{
+	for (int i = 0; i < TOP; ++i)
+	{
+		mSequence[i] = sequence[i];
+	}
+}
+
+// Display the array
+void CPattern::Display()
+{
+	for (int i = TOP; i < TOP; ++i)
+	{
+		cout << mSequence[i] << " ";
+	}
+	cout << endl;
+}
+
+// Display the array in reverse order
+void CPattern::DisplayReversed()
+{
+	for (int i = TOP; i > 0; --i)
+	{
+		cout << mSequence[i] << " ";
+	}
+	cout << endl;
+}
+
+// Find maximum value in the array
+int CPattern::FindMax()
+{
+	int max = -100;
+
+	for (int i = 0; i < TOP; ++i)
+	{
+		if (i > max)
+		{
+			max = i;
+		}
+	}
+	return max;
+}
+
+// Find the location of the first occurence of a value, i.e. it's index
+// Return -1 if not found
+int CPattern::FindLocation(int value)
+{
+	int found = -1;
+	for (int i = 0; i < TOP; ++i)
+	{
+		if (found == mSequence[i])
+		{
+			return found;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	CPattern* myPattern = new CPattern;
+	int testArray[TOP] = { 2, 4, 5, 7, 1, 3 };
+	myPattern->Initialise(testArray);
+
+	cout << "Display the array:" << endl;
+	myPattern->Display();
+
+	cout << endl << "Display the array in reverse order:" << endl;
+	myPattern->DisplayReversed();
+
+	cout << endl << "max element of the array: " << myPattern->FindMax();
+
+	cout << endl << endl;
+	cout << "location of the number 7: " << myPattern->FindLocation(7);
+	cout << endl;
+
+	system("pause");
+}
